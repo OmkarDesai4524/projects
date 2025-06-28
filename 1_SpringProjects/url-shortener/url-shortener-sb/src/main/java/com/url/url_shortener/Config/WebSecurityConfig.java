5352cc02446de053f2d558cf9a38acbdc421446f
OmkarDesai45 <omkar11803@gmail.com>
2025-06-28 19:58:12 +0530

    addition of fitnessApp project

--- 1_SpringProjects/url-shortener/url-shortener-sb/src/main/java/com/url/url_shortener/Config/WebSecurityConfig.java
+++ 1_SpringProjects/url-shortener/url-shortener-sb/src/main/java/com/url/url_shortener/Config/WebSecurityConfig.java
@@ -2,9 +2,11 @@ package com.url.url_shortener.Config;
 
 
 import com.url.url_shortener.security.jwt.JwtAuthenticationFilter;
+import com.url.url_shortener.services.UserDetailsServiceImpl;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -18,50 +20,51 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 @AllArgsConstructor
 public class WebSecurityConfig {
 
-     private UserDetailsService userDetailsService;
+    private UserDetailsServiceImpl userDetailsService;
 
-     @Bean
-     public JwtAuthenticationFilter jwtAuthenticationFilter() {
-         return new JwtAuthenticationFilter();
-     }
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() {
+        return new JwtAuthenticationFilter();
+    }
 
-     @Bean
-     public PasswordEncoder passwordEncoder() {
-         return new BCryptPasswordEncoder();
-     }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 
-     @Bean
-     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
-     }
-
-     @Bean
-     public DaoAuthenticationProvider authenticationProvider(){
-         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-         authProvider.setUserDetailsService(userDetailsService);
-         authProvider.setPasswordEncoder(passwordEncoder());
-         return authProvider;
-     }
-
-     @Bean
-     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-         http.csrf(AbstractHttpConfigurer::disable)
-                 .authorizeHttpRequests(auth -> auth
-                         .requestMatchers("/api/auth/**").permitAll()
-                         .requestMatchers("/api/urls/**").authenticated()
-                         .requestMatchers("/{shortUrl}").permitAll()
-                         .anyRequest().authenticated()
-                 );
-         http.authenticationProvider(authenticationProvider());
-         http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
-         return http.build();
-     }
+    }
 
 
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api/urls/**").authenticated()
+                        .requestMatchers("/{shortUrl}").permitAll()
+                        .anyRequest().authenticated()
+                );
+        http.authenticationProvider(authenticationProvider());
+        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
 }
