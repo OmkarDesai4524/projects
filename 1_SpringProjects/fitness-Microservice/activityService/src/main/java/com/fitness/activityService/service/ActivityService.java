deacf6e258620e751a723cb52a508a975cd6e8c5
OmkarDesai45 <omkar11803@gmail.com>
2025-07-01 23:55:46 +0530

    implemented ai service

---
+++ 1_SpringProjects/fitness-Microservice/activityService/src/main/java/com/fitness/activityService/service/ActivityService.java
@@ -0,0 +1,66 @@
+package com.fitness.activityService.service;
+
+import com.fitness.activityService.dto.ActivityRequest;
+import com.fitness.activityService.dto.ActivityResponse;
+import com.fitness.activityService.model.Activity;
+import com.fitness.activityService.repository.ActivityRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class ActivityService {
+
+    private final ActivityRepository activityRepository;
+    private final UserValidationService validationService;
+
+    public ActivityResponse trackActivity(ActivityRequest request) {
+        boolean isValidUser = validationService.validateUser(request.getUserId());
+        if(!isValidUser) {
+            throw new RuntimeException("Invalid user:" + request.getUserId());
+        }
+        Activity activity = Activity.builder()
+                .userId(request.getUserId())
+                .type(request.getType())
+                .duration(request.getDuration())
+                .startTime(request.getStartTime())
+                .additionalMetrics(request.getAdditionalMetrics())
+                .build();
+
+        Activity savedActivity = activityRepository.save(activity);
+        return mapToResponse(savedActivity);
+
+    }
+
+    private ActivityResponse mapToResponse(Activity activity) {
+        ActivityResponse response = new ActivityResponse();
+        response.setId(activity.getId());
+        response.setUserId(activity.getUserId());
+        response.setType(activity.getType());
+        response.setDuration(activity.getDuration());
+        response.setStartTime(activity.getStartTime());
+        response.setAdditionalMetrics(activity.getAdditionalMetrics());
+        response.setCaloriesBurned(activity.getCaloriesBurned());
+        response.setCreatedAt(activity.getCreatedAt());
+        response.setUpdatedAt(activity.getUpdatedAt());
+        return response;
+
+    }
+
+    public List<ActivityResponse> getUserActivity(String userId) {
+
+        List<Activity> activities = activityRepository.findByUserId(userId);
+        return activities.stream()
+                .map(this::mapToResponse)
+                .collect(Collectors.toList());
+    }
+
+    public ActivityResponse getActivityById(String activityId) {
+        return activityRepository.findById(activityId)
+                .map(this::mapToResponse)
+                .orElseThrow(() -> new RuntimeException("Activity not found with id: " + activityId));
+    }
+}
---
+++ 1_SpringProjects/fitness-Microservice/userservice/src/main/java/com/fitness/userservice/controllers/UserController.java
@@ -0,0 +1,37 @@
+package com.fitness.userservice.controllers;
+
+
+import com.fitness.userservice.Dto.RegisterRequest;
+import com.fitness.userservice.Dto.UserResponse;
+import com.fitness.userservice.service.UserService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/users")
+
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserResponse> getUserProfile(@PathVariable String userId) {
+        return ResponseEntity.ok(userService.getUserProfile(userId));
+    }
+
+    @GetMapping("/register")
+    public ResponseEntity<UserResponse> register(@Valid @RequestBody RegisterRequest request) {
+        return ResponseEntity.ok(userService.register(request));
+    }
+
+    @GetMapping("/{userId}/validate")
+    public ResponseEntity<Boolean> validateUser(@PathVariable String userId) {
+        return ResponseEntity.ok(userService.existsByUserId(userId));
+    }
+
+
+
+}
